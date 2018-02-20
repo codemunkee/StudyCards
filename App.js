62a6f5843ca6593e9d5239b3a85fc5b06ed90564
Russ Nealis <rnealis@twilio.com>
2018-02-20 09:47:07 -0800

    added status bar

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { Constants } from 'expo';
 import { getDecks } from './utils/helpers';
 import DeckLinks from './components/DeckLinks';
+
+function StudyStatusBar({ backgroundColor, ...props }) {
+  return (
+    <View style={{backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
 
 export default class App extends React.Component {
   render() {
     const decks = getDecks();
     return (
       <View style={styles.container}>
+        <StudyStatusBar backgroundColor="darkgray" barStyle="light-content" />
         <DeckLinks style={{flex: 1}} decks={decks} />
       </View>
     );
