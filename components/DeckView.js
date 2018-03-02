09639ba62130761b0710375e7b15be7e45d9f73f
Russ Nealis <rnealis@twilio.com>
2018-03-02 06:44:00 -0800

    remove console logging line

--- components/DeckView.js
+++ components/DeckView.js
@@ -36,7 +36,6 @@ class DeckView extends Component {
   }
 
   render() {
-    console.log('STATE', this.state)
     const { deckData, dataLoaded } = this.state;
     return (
       <View style={{ flex: 1 }}>
