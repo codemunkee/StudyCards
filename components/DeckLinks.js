17513210354bffe330e8c6ffea350488b5b16faf
Russ Nealis <rnealis@twilio.com>
2018-03-03 10:02:45 -0800

    link to AddDeck

--- components/DeckLinks.js
+++ components/DeckLinks.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { getDecks } from '../utils/helpers';
-import { NavigationActions } from 'react-navigation';
+import { FontAwesome } from '@expo/vector-icons'
 
 class DeckLinks extends Component {
 
@@ -10,20 +10,15 @@ class DeckLinks extends Component {
     deckList: [],
   }
 
-  handlePress = (data) => {
+  goToDeck = (deckKey) => {
     return () => {
       const  { navigate } = this.props.navigation;
-      navigate('DeckView', data)
+      navigate('DeckView', deckKey);
     }
   }
 
-  reset() {
-    return this.props.navigation.dispatch(
-      NavigationActions.reset({
-        index: 0,
-        actions: [ NavigationActions.navigate({ routeName: 'Home'})]
-      })
-    );
+  addDeck = () => {
+    this.props.navigation.navigate('AddDeck');
   }
 
   componentDidMount() {
@@ -38,24 +33,53 @@ class DeckLinks extends Component {
     const { deckList } = this.state;
     console.log('DeckLinks (deckList):', deckList);
     return (
-      <FlatList
-          data={deckList}
-          renderItem={({ item }) => (
-              <ListItem
-                containerStyle={styles.container}
-                key={item.title}
-                title={item.title}
-                badge={{ value: item.questions.length, textStyle: { color: 'powderblue' } }}
-                onPress={this.handlePress(item.title)}
-              />
-          )}
-      />
+      <View>
+        <TouchableOpacity
+          style={styles.addDeckContainer}
+          onPress={this.addDeck}
+        >
+          <Text style={styles.addDeckText}>
+            Add a Deck
+          </Text>
+          <View style={{ marginLeft: 5}}>
+            <FontAwesome
+              name="plus"
+              size={20}
+              color="#2a2a66"
+            />
+          </View>
+        </TouchableOpacity>
+        <FlatList
+            data={deckList}
+            renderItem={({ item }) => (
+                <ListItem
+                  containerStyle={styles.listContainer}
+                  key={item.title}
+                  title={item.title}
+                  badge={{ value: item.questions.length, textStyle: { color: 'powderblue' } }}
+                  onPress={this.goToDeck(item.key)}
+                />
+            )}
+        />
+      </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  addDeckContainer: {
+    height: 50,
+    backgroundColor: 'lightgreen',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: 'darkgray',
+    borderWidth: 2,
+    flexDirection: 'row',
+  },
+  addDeckText: {
+    fontSize: 20,
+  },
+  listContainer: {
     flex: 1,
     justifyContent: 'flex-start',
     backgroundColor: 'lightgray',
